d9683538232d3757b8a745bd6a967906344ec1cc
Abdurahman Gugssa (Wipro Ltd.) <v-abdgug@microsoft.com>
2017-09-11 15:29:36 -0700

    Init commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyApJINyWXZ_zGv68uMYp6PCHaH2HLOzU6s',
+    authDomain: 'oshop-93d49.firebaseapp.com',
+    databaseURL: 'https://oshop-93d49.firebaseio.com',
+    projectId: 'oshop-93d49',
+    storageBucket: '',
+    messagingSenderId: '396755345432'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyApJINyWXZ_zGv68uMYp6PCHaH2HLOzU6s',
+    authDomain: 'oshop-93d49.firebaseapp.com',
+    databaseURL: 'https://oshop-93d49.firebaseio.com',
+    projectId: 'oshop-93d49',
+    storageBucket: '',
+    messagingSenderId: '396755345432'
+  }
 };
