81f6ae0910ac346596ebcdd0b01c1ecfc0b910d6
Abdurahman Gugssa (Wipro Ltd.) <v-abdgug@microsoft.com>
2017-09-22 15:56:56 -0700

    Adding data-table and finishing up PMM

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,3 +1,4 @@
+import { DataTableResource } from 'angular-4-data-table';
 import { Product } from '../../models/product';
 import { Subscription } from 'rxjs/Rx';
 import { ProductService } from '../../product.service';
@@ -10,16 +11,40 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
-  filteredProducts: Product[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
 
   constructor(private productService: ProductService) {
-    this.subscription = this.productService.getAll().subscribe(products => this.filteredProducts = this.products = products);
+    this.subscription = this.productService.getAll().subscribe(products => {
+     this.products = products;
+
+      this.initializeTable(products);
+    });
+   }
+
+   private initializeTable (products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 0 })
+      .then(items => this.items = items);
+      this.tableResource.count()
+        .then(count => this.itemCount = count);
+   }
+
+   reloadItems(params) {
+     if (!this.tableResource) {
+      return;
+     }
+    this.tableResource.query(params)
+    .then(items => this.items = items);
    }
 
    filter(query: string) {
-     this.filteredProducts = (query) ?
+     let filteredProducts = (query) ?
        this.products.filter(p => p.title.toLowerCase().includes(query.toLocaleLowerCase())) : this.products;
+
+     this.initializeTable(filteredProducts);
    }
 
    ngOnDestroy() {
